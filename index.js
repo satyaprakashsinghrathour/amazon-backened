8bae054f9cb5ce9997e9449c6e8f8a6cf12d7d57
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-11-06 08:29:54 -0400

    user test successfull

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
  const express = require('express')
  const app = express()
  const port = 3000
-const mongoose = require('mongoose')
- 
+ const mongoose = require('mongoose')
+ const dotenv = require('dotenv')
+ const userRoute = require("./routes/user")
+ dotenv.config();
+
+ mongoose.connect(process.env.MONGO_URL).then(() => {
+     console.log('success__')
+ }).catch((e) => {
+     console.log(e)
+ });
+ //  app.get("/clint", (req, res) => res.send("test is successful"));
+ app.use("/main", userRoute);
+
  app.get('/', (req, res) => res.send('Hello World!'))
  app.listen(port, () => {
      console.log(`server is running`)
